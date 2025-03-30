c8f53778f65d4c284917eb7805beb9ac2502b387
akatakprime <43159805+akatakprime@users.noreply.github.com>
2025-03-29 18:45:20 -0700

    included cors

--- src/server/main.js
+++ src/server/main.js
@@ -15,7 +15,7 @@ import {new_acc, login_attempt, send_cookies, logout, save_set, send_sets, load_
 import cors from 'cors';
 const corsOptions = {
     //make sure you delete the extra slash at the end:
-    origin: ["http://localhost:3000","http://localhost:5173", "https://language-learning-blush.vercel.app", "language-learning-integrated.vercel.app"],
+    origin: ["http://localhost:3000","http://localhost:5173", "https://language-learning-two.vercel.app"],
     credentials: true //be able to send cookies
 };
 
