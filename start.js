142628f442dc3e7fe2f6dc9b7d26d9d1816007e7
akatakprime <43159805+akatakprime@users.noreply.github.com>
2025-03-29 21:50:04 -0700

    vercel.json

---
+++ start.js
@@ -0,0 +1,13 @@
+import child_process from 'child_process'
+
+child_process('npm run start', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
